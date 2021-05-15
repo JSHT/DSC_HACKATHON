fd3504474bcc9afb620daeb3daf8aad96a9daf6f
soda <adcf3016@gmail.com>
2021-05-16 00:29:34 +0800

    oh I forgot delete

--- test/angular-9-firebase-chat-example/src/app/app.component.ts
+++ test/angular-9-firebase-chat-example/src/app/app.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Injectable } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase';
 import { DatePipe } from '@angular/common';
-import * as fa from 'firebase/app'
 
 const config = {
   apiKey: 'jYNA5xUBOyCZUoXoQYyxgtRESXHtoX6ImimUo5UW',
   databaseURL: 'https://chat-list-c2972-default-rtdb.firebaseio.com'
 };
-
-user: b.User
 
 
 @Component({
@@ -24,7 +21,6 @@ export class AppComponent {
 
   constructor() {
     firebase.initializeApp(config);
-    console.log(b)
   }
 
 }
