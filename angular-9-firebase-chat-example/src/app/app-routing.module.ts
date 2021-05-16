fcc3ae12c5c56e28d34d09e61c6f7a3604f7a56d
Jerry Tang <xburnerg6aaa08@gmail.com>
2021-05-16 12:13:16 +0800

    just

---
+++ angular-9-firebase-chat-example/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { AddroomComponent } from './addroom/addroom.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'roomlist', component: RoomlistComponent },
+  { path: 'addroom', component: AddroomComponent },
+  { path: 'chatroom/:roomname', component: ChatroomComponent },
+  // {
+  //   path: '', component: AppComponent,
+  //   // redirectTo: '/login',
+  //   // pathMatch: 'full'
+  // }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular-9-firebase-chat-example/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ElementRef, ViewChild, Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as firebase from 'firebase';
+import { DatePipe } from '@angular/common';
+
+const config = {
+  apiKey: 'jYNA5xUBOyCZUoXoQYyxgtRESXHtoX6ImimUo5UW',
+  databaseURL: 'https://chat-list-c2972-default-rtdb.firebaseio.com'
+};
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-chat';
+  a = new Date();
+
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
+
+}
